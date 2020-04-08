66f81fa7328282a0ec250ef68d5557555524daec
ev.myshko <ev.myshko@tensor.ru>
2020-04-08 15:54:43 +0300

    Исправлено экранирование

--- app/identidock.py
+++ app/identidock.py
@@ -2,7 +2,7 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
-import logging
+import html
 
 app = Flask(__name__)
 salt = "UNIQUE_SALT"
@@ -13,7 +13,7 @@ cache = redis.StrictRedis(host='redis', port=6379, db=0)
 def mainpage():
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+        name = html.escape(request.form['name'], quote=True)
     
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
@@ -32,6 +32,7 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicons(name):
+    name = html.escape(name, quote=True)
     image = cache.get(name)
     if not image:
         resp = requests.get(f'http://dnmonster:8080/monster/{name}?size=80')
