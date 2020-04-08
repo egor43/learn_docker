cd59a6a500fc8d74a6c6cdaa31cd9e0d5eae0681
ev.myshko <ev.myshko@tensor.ru>
2020-04-08 15:54:55 +0300

    Исправлен скрипт запуска

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,9 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
     echo "Running Development server"
     exec python "identidock.py"
+elif [ "$ENV" = 'UNIT_TEST' ]; then
+    echo "Running unit tests"
+    exec python "tests.py"
 else
     echo "Running Production server"
     exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
